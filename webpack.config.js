8d4b72b2ff636d00e8403728144ed0229f7098df
Fábio <fsmaiorano@gmail.com>
2018-03-27 21:21:36 -0300

    add: first commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+  });
+
+module.exports = {
+    entry: [
+        '@babel/polyfill',
+        './index.js',
+    ],
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js',
+    },
+    devServer: {
+        inline: true,
+        contentBase: __dirname + '/public',
+        port: 3000,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader",
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+    ]
+}
+
